6c0988b533e097462cab9bc01ae6a69af1d85f10
Maxim <maxim.tickhonovich@gmail.com>
2019-02-26 23:28:31 +0300

    simple_app

---
+++ SYS_03/WF_SYS_03/WF_SYS_03/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WF_SYS_03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axActiveXCPP21 = new AxActiveXCPP2Lib.AxActiveXCPP2();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.axActiveXCPP21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axActiveXCPP21
+            // 
+            this.axActiveXCPP21.Enabled = true;
+            this.axActiveXCPP21.Location = new System.Drawing.Point(198, 260);
+            this.axActiveXCPP21.Name = "axActiveXCPP21";
+            this.axActiveXCPP21.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axActiveXCPP21.OcxState")));
+            this.axActiveXCPP21.Size = new System.Drawing.Size(100, 50);
+            this.axActiveXCPP21.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(397, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 56);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "output -5";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(48, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 55);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "площадь";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(232, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Введите радиус:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(519, 352);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.axActiveXCPP21);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axActiveXCPP21)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxActiveXCPP2Lib.AxActiveXCPP2 axActiveXCPP21;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SYS_03/WF_SYS_03/WF_SYS_03/Form1.cs
@@ -0,0 +1,58 @@
+﻿using AxActiveXCPP2Lib;
+using ActiveXCPP2Lib;   
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_SYS_03
+{
+    public partial class Form1 : Form
+    {
+        static int k = 10;
+        public Form1()
+        {
+            InitializeComponent();
+            k = axActiveXCPP21.TestMyMethod(4, 4, "Hi");
+            axActiveXCPP21.EventHandlerTest +=
+              new AxActiveXCPP2Lib._DActiveXCPP2Events_EventHandlerTestEventHandler(
+              axActiveXCPP21_EventHandlerTest);
+        }
+
+
+        private void axActiveXCPP21_EventHandlerTest(object sender, _DActiveXCPP2Events_EventHandlerTestEvent e)
+        {
+            int age = e.age;
+            string name = e.name;
+            string surname = e.surname;
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Active returned:" + k);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                float k = 0.0f;
+                k = axActiveXCPP21.TestMyMethod2(Convert.ToUInt32(textBox1.Text));
+                label1.Text += $"результат {k}\n";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
